e1d7cd8a436eb871663c0f275085a4c97d6e854e
WhiteLightningGun <109026202+WhiteLightningGun@users.noreply.github.com>
2024-01-18 21:40:50 +0000

    creating a save point here

---
+++ BackendTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace BackendTests;
+using Backend.Controllers;
+using Xunit;
+using Backend.Data;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        AlbumEntry album = new AlbumEntry();
+
+    }
+}
