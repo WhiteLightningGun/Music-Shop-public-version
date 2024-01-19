af1656ad37586a6ae5a0e0e3eb03f656b0444d4f
WhiteLightningGun <109026202+WhiteLightningGun@users.noreply.github.com>
2024-01-19 01:00:03 +0000

    Refactor data context and remove test files

--- Backend/Data/IDataContext.cs
+++ Backend/Data/IDataContext.cs
@@ -1,16 +1,33 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Backend.Data;
 using Microsoft.EntityFrameworkCore;
-
-namespace Backend.Data;
 
 public interface IDataContext
 {
-    public DbSet<AlbumEntry>? AlbumEntries { get; set; }
-    public DbSet<SongData>? songData { get; set; }
-    public DbSet<UserSongPurchases>? UserSongPurchases { get; set; }
-    public DbSet<UserAlbumPurchases>? UserAlbumPurchases { get; set; }
-    public DbSet<PaypalOrder>? PaypalOrders { get; set; }
-    public DbSet<IdentityUser>? Users { get; set; }
-    public DbSet<IdentityRole>? Roles { get; set; }
+    DbSet<AlbumEntry>? AlbumEntries { get; set; }
+    DbSet<SongData>? songData { get; set; }
+    DbSet<UserSongPurchases>? UserSongPurchases { get; set; }
+    DbSet<UserAlbumPurchases>? UserAlbumPurchases { get; set; }
+    DbSet<PaypalOrder>? PaypalOrders { get; set; }
+    int SaveChanges();
+}
+
+public class DataContextWrapper : IDataContext
+{
+    private readonly DataContext _context;
+
+    public DataContextWrapper(DataContext context)
+    {
+        _context = context;
+    }
+
+    public DbSet<AlbumEntry>? AlbumEntries { get => _context.AlbumEntries; set => _context.AlbumEntries = value; }
+    public DbSet<SongData>? songData { get => _context.songData; set => _context.songData = value; }
+    public DbSet<UserSongPurchases>? UserSongPurchases { get => _context.UserSongPurchases; set => _context.UserSongPurchases = value; }
+    public DbSet<UserAlbumPurchases>? UserAlbumPurchases { get => _context.UserAlbumPurchases; set => _context.UserAlbumPurchases = value; }
+    public DbSet<PaypalOrder>? PaypalOrders { get => _context.PaypalOrders; set => _context.PaypalOrders = value; }
+
+    public int SaveChanges()
+    {
+        return _context.SaveChanges();
+    }
 }
